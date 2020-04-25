1222048ca1fbf431dbce63b032ac60a9bc402aaa
Fischetti Julian <jfischetti94@gmail.com>
2020-04-25 02:08:34 -0300

    delete src/app.js add index.js

--- src/server.js
+++ src/server.js
@@ -1,6 +1,29 @@
-const http = require('http');
-const app = require('./app');
+var cors = require('cors')
+const express = require('express');
+const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3000;
-const server = http.createServer(app);
-server.listen(port);
+const app = express();
+
+// Middleware
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: 'application/json' }));
+
+// Rutas
+app.use('/hi', (req, res, next) => {res.status(200).json({sucsess: "asd"})});
+
+// Error Handling
+app.use((req, res, next) => {
+  const error = new Error('Nof Found');
+  error.status = 404;
+  next(error);
+});
+
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
+});
