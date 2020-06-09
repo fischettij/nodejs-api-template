42325023cbfb2eba1cc415587dbbdf081fa1a4a9
Fischetti Julian <jfischetti94@gmail.com>
2020-06-09 01:02:08 -0300

    fix lint

--- test/server.test.js
+++ test/server.test.js
@@ -1,18 +1,13 @@
 require('dotenv').config({ path: `${__dirname}/../.env.test` });
-require('./jest-extensions');
-const request = require('supertest')
-const app = require('../src/server')
+const request = require('supertest');
 const { NOT_FOUND } = require('http-status-codes');
+const app = require('../src/server');
 
 describe('Hi Endpoints', () => {
-  const resposeSchema = {
-    result: { type: 'string' }
-  }
-
   it('should response 404 when request invalid url', async () => {
-    var res = await request(app)
-    .get('/invalidURL')
-    .send()
-  expect(res.status).toBe(NOT_FOUND)
-  })
-})
+    const res = await request(app)
+      .get('/invalidURL')
+      .send();
+    expect(res.status).toBe(NOT_FOUND);
+  });
+});
