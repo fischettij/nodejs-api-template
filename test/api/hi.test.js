02d4db34accea34931d592f2b8cdd6fb2a1fbb80
Fischetti Julian <jfischetti94@gmail.com>
2020-06-09 01:25:52 -0300

    fix lint

--- test/api/hi.test.js
+++ test/api/hi.test.js
@@ -4,10 +4,6 @@ const { OK } = require('http-status-codes');
 const app = require('../../src/server');
 
 describe('Hi Endpoints', () => {
-  const resposeSchema = {
-    result: { type: 'string' },
-  };
-
   it('/hi', async () => {
     const res = await request(app)
       .get('/hi')
